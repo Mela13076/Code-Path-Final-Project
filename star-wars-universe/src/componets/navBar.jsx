09251443ba85af7945cbcf4444c6903b63866175
Amelia Trevino <trevinoamelia25@gmail.com>
2023-07-29 19:17:43 -0500

    New Navigation Bar

--- star-wars-universe/src/componets/navBar.jsx
+++ star-wars-universe/src/componets/navBar.jsx
@@ -1,19 +1,58 @@
+// import { Link } from 'react-router-dom';
+// import './navBar.css'
+// import Logo from '../assets/logo.png'
+
+
+// function Navigation(){
+//     return(
+//     <header className="header">
+//         <img src={Logo} alt="logo"  height="70px" width="auto"/>
+//         <nav>
+//             <ul>
+//                 <li><Link to="/">Home</Link></li>
+//                 <li><Link to="/create">Create Archive</Link></li>
+//                 <li><Link to="/gallery">Archive History</Link></li>
+//             </ul>
+//         </nav>
+//     </header>
+//     );
+// }
+
+// export default Navigation;
+
+
 import { Link } from 'react-router-dom';
 import './navBar.css'
 import Logo from '../assets/logo.png'
+import React, { useState } from 'react';
 
 
 function Navigation(){
+
+    const [isMenuOpen, setIsMenuOpen] = useState(false);
+      
+    const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+    };
+
     return(
     <header className="header">
-        <img src={Logo} alt="logo"  height="70px" width="auto"/>
-        <nav>
+        <div className="logo">
+            <img src={Logo} alt="logo"  height="70px" width="auto"/>
+        </div>
+        
+        <div className={`menu ${isMenuOpen ? 'open' : 'closed'}`}>
             <ul>
                 <li><Link to="/">Home</Link></li>
                 <li><Link to="/create">Create Archive</Link></li>
                 <li><Link to="/gallery">Archive History</Link></li>
             </ul>
-        </nav>
+        </div>
+            <div className={`hamburger ${isMenuOpen ? 'open' : ''}`} onClick={toggleMenu}>
+                <span></span>
+                <span></span>
+                <span></span>
+            </div>
     </header>
     );
 }
